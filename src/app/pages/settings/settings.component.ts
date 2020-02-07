b96d6af6a50816a423c5a4e34472f6bab565a41b
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-02-07 23:17:26 +0200

    Implemented logic for duplication and deletion of templates

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { ModalService } from '../../services/modal.service';
 import { MultipleFileUploaderService } from '../../services/multiple-file-uploader.service';
 
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -20,6 +21,7 @@ export class SettingsComponent implements OnInit {
 
   settingsModel: any = {};
   ci_template: null
+  new_template_name: null
 
   constructor(
     private api: ApiService,
@@ -32,29 +34,90 @@ export class SettingsComponent implements OnInit {
     this.auth.getUser().then((user) => {
       this.switchSetting('General');
 
-      this.api.get(`ci_template/listAll`).then((resp) => {
-        this.settingsModel.ci_template_list = resp.data;
-      });  
+      this.updateCITemplatesList();
     }, (err) => {
       this.route.navigate(['signin']);
     });
   }
 
-  dublicateScript(){
-    
+  updateCITemplatesList() {
+    this.api.get(`ci_template/listAll`).then((resp) => {
+      this.settingsModel.ci_template_list = resp.data;
+    });  
   }
 
-  templateSelected(){
-    console.log(this.settingsModel.ci_template);
+  templateSelected() {
     this.api.get(`ci_template/download/${this.settingsModel.ci_template}`).then((resp) => {
       this.settingsModel.ci_template_script = resp.data;
     });  
-
-    console.log(this.settingsModel.ci_template_script);
   }
 
-  saveScript(){
-    console.log("Save pressed!");
+  duplicateCITemplate() {
+    this.settingsModel.ci_template_list.forEach(item => {
+      if (item == this.settingsModel.new_template_name){
+        this.modal.alert(`Template with this name already exist, please change the name`);
+        return;
+      }
+    });
+
+    this.api.create(`ci_template/${this.settingsModel.new_template_name}`, this.settingsModel.ci_template_script).then((resp) => {
+      if (resp.status == "ok")  {
+        this.settingsModel.new_template_name = "";
+        this.settingsModel.ci_template = resp.data;
+        this.templateSelected();
+        this.updateCITemplatesList();
+      }
+    });
+  }
+
+  deleteCITemplate(){
+    //Ask to delete CI template
+    this.modal.confirm(
+      `Confirm deletion of "${this.settingsModel.ci_template}" template`,
+      "Do you really want to delete this template?<br>If yes, please input template name.",
+      (value) => {
+        if(value !== this.settingsModel.ci_template)
+          return 'Template name is incorrect!';
+      },
+      'Yes, please remove!',
+      'Don`t remove'
+  ).then((res) => {
+    this.api.remove(`ci_template/${this.settingsModel.ci_template}`).then((resp) => {
+      if (resp.status == "ok")  {
+        this.settingsModel.new_template_name = "";
+        this.settingsModel.ci_template_script = "";
+        this.settingsModel.ci_template = null;
+        this.updateCITemplatesList();
+      }
+    });
+    }, (err) => {
+      this.modal.alert(err);
+    })
+  }
+
+  saveScript() {
+    this.modal.confirm(
+      `Confirm deletion of "${this.settingsModel.ci_template}" template`,
+      "Do you really want to delete this template?<br>If yes, please input template name.",
+      (value) => {
+        if(value !== this.settingsModel.ci_template)
+          return 'Template name is incorrect!';
+      },
+      'Yes, please remove!',
+      'Don`t remove'
+  ).then((res) => {
+    this.api.create(`ci_template/${this.settingsModel.ci_template}`, this.settingsModel.ci_template_script).then((resp) => {
+      if (resp.status == "ok")  {
+        this.settingsModel.new_template_name = "";
+        this.settingsModel.ci_template = resp.data;
+        this.templateSelected();
+        this.updateCITemplatesList();
+      }
+    });
+
+    }, (err) => {
+      this.modal.alert(err);
+    })
   }
   
   switchSetting(page) {
