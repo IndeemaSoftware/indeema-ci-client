83b4553297b5258593d258ac16715aeffda7e677
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-02-08 01:37:23 +0200

    Added management of OS systems

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -3,7 +3,6 @@ import { AuthService } from '../../services/auth.service';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
 import { ModalService } from '../../services/modal.service';
-import { MultipleFileUploaderService } from '../../services/multiple-file-uploader.service';
 
 
 @Component({
@@ -12,8 +11,6 @@ import { MultipleFileUploaderService } from '../../services/multiple-file-upload
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  //File uploader
-  public uploader: MultipleFileUploaderService;
 
   modelDefault: any = {
     project_path: "/home/$USER",
@@ -22,6 +19,9 @@ export class SettingsComponent implements OnInit {
   settingsModel: any = {};
   ci_template: null
   new_template_name: null
+
+  server: null
+  new_server_name: null
 
   constructor(
     private api: ApiService,
@@ -34,12 +34,15 @@ export class SettingsComponent implements OnInit {
     this.auth.getUser().then((user) => {
       this.switchSetting('General');
 
+      this.updateServersList();
       this.updateCITemplatesList();
     }, (err) => {
       this.route.navigate(['signin']);
     });
   }
 
+//Working with CI templates
+//Start
   updateCITemplatesList() {
     this.api.get(`ci_template/listAll`).then((resp) => {
       this.settingsModel.ci_template_list = resp.data;
@@ -95,7 +98,7 @@ export class SettingsComponent implements OnInit {
     })
   }
 
-  saveScript() {
+  saveTemplateScript() {
     this.modal.confirm(
       `Confirm saving of updates of "${this.settingsModel.ci_template}" script`,
       "Do you really want to save changes of script?<br>If yes, please input template name.",
@@ -119,7 +122,93 @@ export class SettingsComponent implements OnInit {
       this.modal.alert(err);
     })
   }
-  
+//Finish
+//Working with CI templates
+
+//Working with Servers
+//Start
+updateServersList() {
+  this.api.get(`servers/listAll`).then((resp) => {
+    this.settingsModel.server_list = resp.data;
+  });  
+}
+
+serverSelected(){
+  this.api.get(`servers/download/${this.settingsModel.server}`).then((resp) => {
+    this.settingsModel.server_script = resp.data;
+  });  
+}
+
+duplicateServer(){
+  this.settingsModel.server_list.forEach(item => {
+    if (item == this.settingsModel.new_server_name){
+      this.modal.alert(`Template with this name already exist, please change the name`);
+      return;
+    }
+  });
+
+  this.api.create(`servers/${this.settingsModel.new_server_name}`, this.settingsModel.server_script).then((resp) => {
+    if (resp.status == "ok")  {
+      this.settingsModel.new_server_name = "";
+      this.settingsModel.server = resp.data;
+      this.serverSelected();
+      this.updateServersList();
+    }
+  });}
+
+deleteServer() {
+  this.modal.confirm(
+    `Confirm deletion of "${this.settingsModel.server}" template`,
+    "Do you really want to delete this template?<br>If yes, please input template name.",
+    (value) => {
+      if(value !== this.settingsModel.server)
+        return 'Template name is incorrect!';
+    },
+    'Yes, please remove!',
+    'Don`t remove'
+).then((res) => {
+  this.api.remove(`servers/${this.settingsModel.server}`).then((resp) => {
+    if (resp.status == "ok")  {
+      this.settingsModel.new_server_name = "";
+      this.settingsModel.server_script = "";
+      this.settingsModel.server = null;
+      this.updateServersList();
+    }
+  });
+  }, (err) => {
+    this.modal.alert(err);
+  })
+}
+
+saveServerScript(){
+  this.modal.confirm(
+    `Confirm saving of updates of "${this.settingsModel.server}" script`,
+    "Do you really want to save changes of script?<br>If yes, please input template name.",
+    (value) => {
+      if(value !== this.settingsModel.server)
+        return 'Template name is incorrect!';
+    },
+    'Yes, please save!',
+    'Don`t save'
+).then((res) => {
+  this.api.create(`servers/${this.settingsModel.server}`, this.settingsModel.server_script).then((resp) => {
+    if (resp.status == "ok")  {
+      this.settingsModel.new_server_name = "";
+      this.settingsModel.server = resp.data;
+      this.serverSelected();
+      this.updateServersList();
+    }
+  });
+
+  }, (err) => {
+    this.modal.alert(err);
+  })
+}
+//Finish
+//Working with Servers
+
+//Tab bar navigation
+//Start
   switchSetting(page) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -139,4 +228,6 @@ export class SettingsComponent implements OnInit {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(page).style.display = "block";
   }
+//Finish
+//Tab bar navigation
 }
