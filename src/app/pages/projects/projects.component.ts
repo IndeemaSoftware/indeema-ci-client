bc83481146ab1bc3062f445571d3f99d6b9de0a9
Denis Stoliarchuk <denis.stoliarchuk@indeema.com>
2020-05-26 19:08:44 +0300

    Refactor project and servers(without update design)

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -31,97 +31,123 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit() {
     this.auth.getUser().then((user) => {
-      this.checkUser();
+      this.checkGuide();
       this.getProjects();
     }, (err) => {
       this.route.navigate(['signin']);
     });
   }
 
-  checkUser() {
-    this.api.get(`services`).then((resp) => {
-      if (resp.length <= 0) {
+  /**
+   * Check if user have avaliable guide to show
+   */
+  checkGuide() {
+    this.api.get(`services`).then((res) => {
+      if (res.length <= 0) {
         this.modal.guide();
       }
     });  
   }
 
+  /**
+   * Cleanup models before update
+   */
+  cleanup(){
+    this.statuses = {
+      waiting: 0,
+      progress: 0,
+      success: 0,
+      failed: 0,
+      cleanup: 0,
+      cleanup_success: 0,
+      cleanup_failed: 0
+    };
+  }
+
+  /**
+   * Get list of project and update counters
+   */
   getProjects(){
     this.api.get('projects').then((projects) => {
       this.projects = projects;
 
-      this.statuses = {
-        waiting: 0,
-        progress: 0,
-        success: 0,
-        failed: 0,
-        cleanup: 0,
-        cleanup_success: 0,
-        cleanup_failed: 0
-      };
+      this.cleanup();
 
       //Setup counters
       for(let project of this.projects){
         if(project.apps && project.apps.length){
           for(let app of project.apps){
-            if(app.app_status === 'waiting')
-              this.statuses.waiting++;
-
-            if(app.app_status === 'progress')
-              this.statuses.progress++;
-
-            if(app.app_status === 'success')
-              this.statuses.success++;
-
-            if(app.app_status === 'failed')
-              this.statuses.failed++;
-
-            if(app.app_status === 'cleanup')
-              this.statuses.cleanup++;
-
-            if(app.app_status === 'cleanup_success')
-              this.statuses.cleanup_success++;
-
-            if(app.app_status === 'cleanup_failed')
-              this.statuses.cleanup_failed++;
+            switch(app.app_status){
+              case 'waiting':
+                this.statuses.waiting++;
+                break;
+              case 'progress':
+                this.statuses.progress++;
+                break;
+              case 'success':
+                this.statuses.success++;
+                break;
+              case 'failed':
+                this.statuses.failed++;
+                break;
+              case 'cleanup':
+                this.statuses.cleanup++;
+                break;
+              case 'cleanup_success':
+                this.statuses.cleanup_success++;
+                break;
+              case 'cleanup_failed':
+                this.statuses.cleanup_failed++;
+                break;
+              default:
+                break;
+            }
           }
         }
       }
 
     }, (err) => {
-      console.error(err);
       this.projects = [];
-
-      this.statuses = {
-        waiting: 0,
-        progress: 0,
-        success: 0,
-        failed: 0,
-        cleanup: 0,
-        cleanup_success: 0,
-        cleanup_failed: 0
-      };
+      this.cleanup();
     })
   }
 
-  startProject(project, app){
+  /**
+   * Proceed to start project setup
+   * @param project
+   * @param app
+   */
+  setupProject(project, app){
     if(app.app_status === 'progress' || app.app_status === 'cleanup')
       return;
 
-    this.route.navigate([`console/${project.id}/${app.id}`], { queryParams: { start: 'true' } });
+    this.toConsole(project, app, false, true);
   }
 
+  /**
+   * Redirect method for console view
+   * @param project
+   * @param app
+   * @param cleanup
+   * @param autostart
+   */
   toConsole(project, app, cleanup = false, autostart = false){
-    if(cleanup)
-      this.route.navigate([`console/${project.id}/${app.id}`], { queryParams: { start: autostart, cleanup: 'true' } });
-    else
-      this.route.navigate([`console/${project.id}/${app.id}`]);
+    this.route.navigate([`console/${project.id}/${app.id}`], { queryParams: { start: autostart, cleanup: cleanup } });
   }
 
+  /**
+   * Go to project edit view
+   * @param project
+   */
   editProject(project){
     this.route.navigate([`projects/${project.id}`]);
   }
 
+  /**
+   * Start to cleanup project
+   * @param project
+   * @param app
+   */
   cleanupProject(project, app){
     if(app.app_status === 'cleanup' || app.app_status === 'cleanup_success')
       return;
@@ -136,32 +162,27 @@ export class ProjectsComponent implements OnInit {
         },
         'Yes, please cleanup!',
         'Don`t cleanup'
-    ).then((res) => {
-      this.toConsole(project, app, true, true);
-    });
+    ).then((res) => this.toConsole(project, app, true, true));
   }
 
+  /**
+   * Delete project method
+   * @param project
+   * @param app
+   */
   deleteProject(project, app = null) {
     if (app) {
       if(app.app_status !== 'cleanup_success' && app.app_status !== 'cleanup_failed'){
         this.modal.alert('Before delete app, please cleanup this app!', 'Important!', 'I understand!');
       } else {
         this.modal.confirm(
-            `Confirm deleting app "${app.app_name}"`,
-            "Do you really want to remove this app?<br>If yes, please input app name.",
-            (value) => {
-              if(value !== app.app_name)
-                return 'App name is incorrect!';
-            }
-        ).then((res) => {
-          this.api.remove(`projects/${project.id}/${app.id}`).then(() => {
-            this.getProjects();
-          }, (err) => {
-          });
-          setTimeout(() => {
-            this.getProjects();
-          }, 1000);
-        });
+          `Confirm deleting app "${app.app_name}"`,
+          "Do you really want to remove this app?<br>If yes, please input app name.",
+          (value) => {
+            if(value !== app.app_name)
+              return 'App name is incorrect!';
+          }
+        ).then((res) => this.deleteProjectAndUpdate(project));
       }
     } else {
       if(!this.isAppsNeedToCleanup(project)){
@@ -172,21 +193,26 @@ export class ProjectsComponent implements OnInit {
               if(value !== project.project_name)
                 return 'Project name is incorrect!';
             }
-        ).then((res) => {
-          this.api.remove(`projects/remove/${project.id}`).then(() => {
-            this.getProjects();
-          }, (err) => {
-          });
-          setTimeout(() => {
-            this.getProjects();
-          }, 1000);
-        });
+        ).then((res) => this.deleteProjectAndUpdate(project));
       }else{
         this.modal.alert('Please first cleanup all apps in project', 'Important!', 'I understand!');
       }
     }
   }
 
+  /**
+   * Delete project request and update
+   * @param project
+   */
+  deleteProjectAndUpdate(project){
+    this.api.remove(`projects/remove/${project.id}`).then(() => this.getProjects());
+    setTimeout(() => this.getProjects(), 1000);
+  }
+
+  /**
+   * Check if apps can cleanup
+   * @param project
+   */
   isAppsNeedToCleanup(project){
     let isNeed = false;
     for(let app of project.apps){
@@ -198,12 +224,13 @@ export class ProjectsComponent implements OnInit {
     return isNeed;
   }
 
+  /**
+   * Go to preview of project app
+   * @param project
+   * @param app
+   */
   preview(project, app) {
-    if (app.isSecure) {
-      window.open(`https://${app.domain_name}:443`,'_blank');
-    } else {
-      window.open(`http://${app.domain_name}:${app.app_port}`,'_blank');
-    }
+    window.open(`http://${app.domain_name}:${(app.isSecure)? '433' : app.app_port}`,'_blank');
   }
 
 }
--- src/app/pages/servers/servers.component.ts
+++ src/app/pages/servers/servers.component.ts
@@ -30,81 +30,96 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
     this.auth.getUser().then((user) => {
-      this.checkUser();
+      this.checkWelcome();
       this.getServers();
     }, (err) => {
       this.route.navigate(['signin']);
     });
   }
 
-  checkUser() {
-    this.api.get(`platforms`).then((resp) => {
-      if (resp.length <= 0) {
+  /**
+   * Check if we need to show welcome message for user
+   */
+  checkWelcome() {
+    this.api.get(`platforms`).then((res) => {
+      if (res.length <= 0) {
         this.modal.alert(`Hi ${this.auth.user.username} and Welcome to Indeema CI. To start setuping servers, first setup platform scripts. You can use set of ready scripts in Settings/Modules. Install sets with one click.`);
       }
     });  
   }
 
+  /**
+   * Cleanup models before update
+   */
+  cleanup(){
+    this.statuses = {
+      waiting: 0,
+      progress: 0,
+      success: 0,
+      failed: 0,
+      cleanup: 0,
+      cleanup_success: 0,
+      cleanup_failed: 0
+    };
+  }
+
+  /**
+   * Get server list and update counters
+   */
   getServers(){
     this.api.get('server').then((servers) => {
       this.servers = servers;
-
-      this.statuses = {
-        waiting: 0,
-        progress: 0,
-        success: 0,
-        failed: 0,
-        cleanup: 0,
-        cleanup_success: 0,
-        cleanup_failed: 0
-      };
+      this.cleanup();
 
       //Setup counters
       for(let server of this.servers){
-        if(server) { 
-          if(server.server_status === 'waiting')
-            this.statuses.waiting++;
-
-          if(server.server_status === 'progress')
-            this.statuses.progress++;
-
-          if(server.server_status === 'success')
-            this.statuses.success++;
-
-          if(server.server_status === 'failed')
-            this.statuses.failed++;
-
-          if(server.server_status === 'cleanup')
-            this.statuses.cleanup++;
-
-          if(server.server_status === 'cleanup_success')
-            this.statuses.cleanup_success++;
-
-          if(server.server_status === 'cleanup_failed')
-            this.statuses.cleanup_failed++;
+        if(server) {
+          switch(server.server_status){
+            case 'waiting':
+              this.statuses.waiting++;
+              break;
+            case 'progress':
+              this.statuses.progress++;
+              break;
+            case 'success':
+              this.statuses.success++;
+              break;
+            case 'failed':
+              this.statuses.failed++;
+              break;
+            case 'cleanup':
+              this.statuses.cleanup++;
+              break;
+            case 'cleanup_success':
+              this.statuses.cleanup_success++;
+              break;
+            case 'cleanup_failed':
+              this.statuses.cleanup_failed++;
+              break;
+            default:
+              break;
+          }
         }
       }
 
     }, (err) => {
-      console.error(err);
       this.servers = [];
-
-      this.statuses = {
-        waiting: 0,
-        progress: 0,
-        success: 0,
-        failed: 0,
-        cleanup: 0,
-        cleanup_success: 0,
-        cleanup_failed: 0
-      };
+      this.cleanup();
     })
   }
 
+  /**
+   * Start to setup server
+   * @param server
+   */
   setupServer(server){
     this.toConsole(server, false, true);
   }
 
+  /**
+   * Start to cleanup server
+   * @param server
+   */
   cleanupServer(server){
     if(server.server_status === 'cleanup' || server.server_status === 'cleanup_success')
       return;
@@ -119,42 +134,62 @@ export class ServersComponent implements OnInit {
         },
         'Yes, please cleanup!',
         'Don`t cleanup'
-    ).then((res) => {
-      this.toConsole(server, true, true);
-    });
+    ).then((res) => this.toConsole(server, true, true));
   }
 
+  /**
+   * Redirect method for console view
+   * @param server
+   * @param cleanup
+   * @param autostart
+   */
   toConsole(server, cleanup = false, autostart = false) {
     this.route.navigate([`console/server/${server.id}`], { queryParams: { start: autostart, cleanup: cleanup } });
   }
 
+  /**
+   * Go to server edit view
+   * @param server
+   */
   editServer(server){
     this.route.navigate([`servers/${server.id}`]);
   }
 
+  /**
+   * Delete server method
+   * @param server
+   */
   deleteServer(server) {
-    if ( server.server_status !== 'cleanup_success' 
-    && server.server_status !== 'cleanup_failed' 
-    && server.server_status !== 'failed'
-    && server.server_status !== 'waiting' ) {
+    if (
+        server.server_status !== 'cleanup_success' &&
+        server.server_status !== 'cleanup_failed' &&
+        server.server_status !== 'failed' &&
+        server.server_status !== 'waiting'
+    ) {
       this.modal.alert('Before delete app, please cleanup this app!', 'Important!', 'I understand!');
-    } else {
-      this.modal.confirm(
-          `Confirm deleting app "${server.server_name}"`,
-          "Do you really want to remove this app?<br>If yes, please input app name.",
-          (value) => {
-            if(value !== server.server_name)
-              return 'App name is incorrect!';
-          }
-      ).then((res) => {
-        this.api.remove(`server/${server.id}`).then(() => {
-          this.getServers();
-        }, (err) => {
-        });
-        setTimeout(() => {
-          this.getServers();
-        }, 1000);
-      });
+      return;
     }
+
+    this.modal.confirm(
+        `Confirm deleting app "${server.server_name}"`,
+        "Do you really want to remove this app?<br>If yes, please input app name.",
+        (value) => {
+          if(value !== server.server_name)
+            return 'App name is incorrect!';
+        }
+    ).then((res) => this.deleteServerAndUpdate(server));
+  }
+
+  /**
+   * Delete server action
+   * @param server
+   */
+  deleteServerAndUpdate(server){
+    this.api.remove(`server/${server.id}`).then(() => {
+      this.getServers();
+    });
+    setTimeout(() => {
+      this.getServers();
+    }, 1000);
   }
 }
